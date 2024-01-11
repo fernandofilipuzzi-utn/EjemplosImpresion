04d2b6fbc1b4736a1ed5732fa1634025f20b59c1
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-01-11 12:39:06 -0300

    inicio

---
+++ Impresion/Margenes/FormPrincipal.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Margenes
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.SuspendLayout();
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Document = this.printDocument1;
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Imprimir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 177);
+            this.Controls.Add(this.button1);
+            this.Name = "FormPrincipal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormPrincipal_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+    }
+}
+
---
+++ Impresion/Margenes/FormPrincipal.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Margenes
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        string PathDirectoryPdf = "";
+        string PathFilePdf = "";
+        private void FormPrincipal_Load(object sender, EventArgs e)
+        {
+            PathDirectoryPdf = Path.GetFullPath(@"salida\");
+            PathFilePdf = Path.Combine(Path.GetDirectoryName(PathDirectoryPdf), "salida.pdf");
+            //lnklbficheroPDF.Text = Path.GetFileName(PathFilePdf);
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+
+            //elegir impresora, papel, margenes de impresion
+            if (printDialog1.ShowDialog() == DialogResult.OK)
+            {
+                printDocument1.PrinterSettings = printDialog1.PrinterSettings;
+                printDocument1.Print();
+            }
+        }
+
+        
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            List<string> personas = new List<string> { "López, Ana María del Carmen",
+                                                        "Martinez, Betiana",
+                                                        "De la Vega, Diego"};
+
+            Graphics g = e.Graphics;
+
+            Pen pen = new Pen(Color.Black);
+
+            /* asociados a la configuración de la pagina.
+            g.PageUnit
+            g.PageScale
+            e.PageSettings.Bounds
+            e.PageSettings.PrintableArea
+            */
+
+            //no siempre la impresora permite elegir los margenes, asique
+            //se puede hacer de otra forma, a partir del tamaño de papel elegido
+            g.DrawRectangle(pen,
+                                e.MarginBounds.Left,
+                                e.MarginBounds.Top,
+                                e.MarginBounds.Width,
+                                e.MarginBounds.Height);
+
+            float x = e.MarginBounds.Left + 10;
+            float y = e.MarginBounds.Top + 20;
+            Font font = new Font("Arial", 12);
+            Brush brush = new SolidBrush(Color.Black);
+
+            foreach (string persona in personas)
+            {
+                y += 20;
+                g.DrawString(persona, font, brush, x, y);
+            }
+        }
+
+        
+    }
+}
